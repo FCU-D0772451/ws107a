f2ad4c72f50b65f94880778b8c27997233999fc2
blackstuend <lin70208@gmail.com>
2018-10-31 20:08:47 +0800

    add blog

---
+++ exercise/blog_add/model.js
@@ -0,0 +1,22 @@
+const M = module.exports = {}
+
+const posts = []
+
+M.add = function (post) {
+  const id = posts.push(post) - 1
+  post.created_at = new Date()
+  post.id = id
+}
+
+M.get = function (id) {
+  return posts[id]
+}
+
+M.list = function () {
+  return posts
+}
+M.modify=function(id,body)
+{
+  posts[id].title=body.title;
+  posts[id].body=body.body;
+}
